c797276bac1e56da5d5a36b5c221e09bda7c7044
Ignas Koncius <i.koncius@gmail.com>
2021-07-05 11:55:58 +0300

    cart item responsive

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -25,12 +25,15 @@ class CartItem extends Component {
           </div>
         </div>
         <div className="cart-col">
+          <h3 className="d-upto-800">Price</h3>
           <h3 className="price">1000 eur</h3>
         </div>
         <div className="cart-col">
+          <h3 className="d-upto-800">Quantity</h3>
           <input className="cart-qty" type="number" value={this.state.qty} onChange={this.handleQty} />
         </div>
         <div className="cart-col">
+          <h3 className="d-upto-800">Total</h3>
           <h3 className="price-total">1000 eur</h3>
         </div>
       </div>
