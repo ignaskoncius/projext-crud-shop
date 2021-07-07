4015e2f861021e5d564aa1cbd0959bc61e038bd0
Ignas Koncius <i.koncius@gmail.com>
2021-07-07 10:08:18 +0300

    price and salePrice done

--- src/components/common/price.jsx
+++ src/components/common/price.jsx
@@ -1,9 +1,22 @@
+import css from './price.module.css';
+
 const Price = (props) => {
   return (
-    <p className="price">
-      {props.children} eur sale price: {props.salePrice}
-    </p>
+    <div className="price">
+      {props.salePrice ? (
+        <p>
+          <span className={css.salePrice}>{formatPrice(props.salePrice)}</span>
+          <span className={css.oldPrice}>{formatPrice(props.children)}</span>
+        </p>
+      ) : (
+        <span>{formatPrice(props.children)}</span>
+      )}
+    </div>
   );
 };
+
+function formatPrice(price) {
+  return price.toFixed(2) + ' eur ';
+}
 
 export default Price;
