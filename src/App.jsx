783f7262a5bb6176ba9b41f001537ca9f03c4d20
Ignas Koncius <i.koncius@gmail.com>
2021-07-08 11:11:26 +0300

    clean state of database data

--- src/App.jsx
+++ src/App.jsx
@@ -16,75 +16,12 @@ class App extends Component {
       { to: '/about', title: 'About' },
     ],
     shop: {
-      shopCategories: [
-        { _id: 1, title: 'Accessories' },
-        { _id: 2, title: 'Denim' },
-        { _id: 3, title: 'Footwear' },
-        { _id: 4, title: 'Jeans' },
-        { _id: 5, title: 'T-Shirts' },
-      ],
+      shopCategories: [],
+      items: [],
       socialLinksData: [
         { to: 'https://www.facebook.com', icon: 'fa fa-facebook', title: 'share' },
         { to: 'https://www.twitter.com', icon: 'fa fa-twitter', title: 'tweet' },
         { to: 'https://www.instagram.com', icon: 'fa fa-instagram', title: 'pin it' },
-      ],
-      items: [
-        {
-          _id: 1,
-          title: 'Redneck hat',
-          price: 99.99,
-          salePrice: 19.99,
-          image: 'acc_hat_01_',
-          color: 'Green',
-          sizeQty: [
-            {
-              size: 'small',
-              quantity: 10,
-            },
-            {
-              size: 'medium',
-              quantity: 5,
-            },
-            {
-              size: 'large',
-              quantity: 15,
-            },
-          ],
-          images: [1, 2, 3, 4, 5],
-          category: 'Accessories',
-        },
-        {
-          _id: 2,
-          title: 'Redneck jeans',
-          price: 1199.99,
-          image: 'jack_jeans_01_',
-          color: 'Yellow',
-          size: ['Small', 'Normal', 'Large'],
-          images: [1, 2, 3, 4, 5, 6],
-          category: 'Jeans',
-        },
-        {
-          _id: 3,
-          title: 'Redneck T-Shirt',
-          price: 369.99,
-          salePrice: 119.99,
-          image: 'shirt_01_',
-          color: 'Gray',
-          size: ['Small', 'Normal', 'Large'],
-          images: [1, 2, 3, 4, 5],
-          category: 'T-Shirts',
-        },
-        {
-          _id: 4,
-          title: 'Cowboy suede boots',
-          price: 299.69,
-          image: 'foot_cowboy_01_',
-          color: 'Sand',
-          size: ['Small', 'Normal', 'Large'],
-          images: [1, 2, 3, 4],
-          quantity: 5,
-          category: 'Footwear',
-        },
       ],
       cart: [
         {
@@ -119,8 +56,13 @@ class App extends Component {
     //   .then((result) => console.log(result.data))
     //   .catch((err) => console.log(err));
     try {
-      const { data } = await axios.get('http://localhost:4000/api/shop/categories');
-      console.log(data);
+      const categoriesResult = await axios.get('http://localhost:4000/api/shop/categories');
+      const itemsResult = await axios.get('http://localhost:4000/api/shop/items');
+
+      const shopCopy = { ...this.state.shop };
+      shopCopy.shopCategories = categoriesResult.data;
+      shopCopy.items = itemsResult.data;
+      this.setState({ shop: shopCopy });
     } catch (err) {
       console.log(err);
     }
